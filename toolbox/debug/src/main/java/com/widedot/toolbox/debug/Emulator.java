9df99b4449d591de0df2f4c8b7e3fafb881617c9
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2022-11-22 00:08:55 +0100

    set position for dcmoto 20221121

--- toolbox/debug/src/main/java/com/widedot/toolbox/debug/Emulator.java
+++ toolbox/debug/src/main/java/com/widedot/toolbox/debug/Emulator.java
@@ -10,7 +10,7 @@ public class Emulator {
 	public static int pid = 0;
 	public static Pointer process = null;
 
-	public static final long ramAddress   = 0x00007F57C0;       // ram[0x80000] ram (maxi 512K pour TO9+) 7F57C0 2022.11.21 7F47C0 2021.04.24
+	public static final long ramAddress   = 0x00007F57C0;       // ram[0x80000] ram (maxi 512K pour TO9+) 2022.11.21
 	public static final long portAddress  = ramAddress+0x80000; // port[0x40]   ports d'entrees/sorties
 	public static final long ddrAddress   = portAddress+0x40;   // ddr[0x40]    registres de direction des PIA 6821
 	public static final long plineAddress = ddrAddress+0x40;    // pline[0x40]  peripheral input-control lines des PIA 6821
