9f4498064cf377602edf9790ed5548dc912ff50f
bhrousseau <bh.rousseau@gmail.com>
2023-07-13 23:55:17 +0200

    wip lwasm

--- toolbox/gamebuilder/src/main/java/com/widedot/m6809/gamebuilder/plugin/lwasm/Processor.java
+++ toolbox/gamebuilder/src/main/java/com/widedot/m6809/gamebuilder/plugin/lwasm/Processor.java
@@ -1,14 +1,20 @@
 package com.widedot.m6809.gamebuilder.plugin.lwasm;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.tree.ImmutableNode;
+import org.apache.commons.io.FileUtils;
 
-import com.widedot.m6809.gamebuilder.configuration.media.Group;
+import com.widedot.m6809.gamebuilder.Settings;
 import com.widedot.m6809.gamebuilder.lwtools.LwAssembler;
+import com.widedot.m6809.gamebuilder.spi.DefaultFactory;
+import com.widedot.m6809.gamebuilder.spi.DefaultPluginInterface;
+import com.widedot.m6809.gamebuilder.spi.FileFactory;
+import com.widedot.m6809.gamebuilder.spi.FilePluginInterface;
 import com.widedot.m6809.gamebuilder.spi.configuration.Defaults;
 import com.widedot.m6809.gamebuilder.spi.configuration.Defines;
 
@@ -24,32 +30,65 @@ public class Processor {
 		defines.add(node);
 		defaults.add(node);
 		
-		List<byte[]> bins = new ArrayList<byte[]>();
 		List<File> files = new ArrayList<File>();
-		Group.recurse(node, path, files);
 		
-		// assemble ressources
-		int length = 0;
-		for (File file : files) {
-			
-			// c'est l'assembleur qui doit appeller la routine adhoc en fonction du build format en entrée
-			// il doit en ressortir un bin pas un obj ...
-			byte[] bin = LwAssembler.assemble(file.getAbsolutePath(), path, defines.values, format);
-			bins.add(bin);
-			length += bin.length; // TODO problème ici ... dépend pas du type
-			// pourquoi on utilise plus resource ??? pour le bin ???
-		}
+   		// instanciate plugins
+		DefaultFactory defaultFactory;
+		FileFactory fileFactory;
+		
+		List<ImmutableNode> root = node.getNodeModel().getNodeHandler().getRootNode().getChildren();
+		for (ImmutableNode child : root) {
+			String plugin = child.getNodeName();
 
-		// concat binaries mais sans l'objet ressource !
-		log.debug("Concat assembled binaries");
-		byte[] finalbin = new byte[length];
-		int i = 0;
-		for (byte[] bin : bins) {
-			System.arraycopy(bin, 0, finalbin, i, bin.length);
-			i += bin.length;
+			// skip non plugins
+			if (plugin.equals("default") ||
+				plugin.equals("define")) continue;
+			
+			List<HierarchicalConfiguration<ImmutableNode>> elements = node.configurationsAt(plugin);
+			for (HierarchicalConfiguration<ImmutableNode> element : elements) {
+				
+				// external plugin
+				defaultFactory = Settings.pluginLoader.getDefaultFactory(plugin);
+			    if (defaultFactory == null) {
+			    	// embeded plugin
+			    	defaultFactory = Settings.embededPluginLoader.getDefaultFactory(plugin);
+			    }
+			    
+				// external plugin
+			    fileFactory = Settings.pluginLoader.getFileFactory(plugin);
+			    if (fileFactory == null) {
+			    	// embeded plugin
+			    	fileFactory = Settings.embededPluginLoader.getFileFactory(plugin);
+			    }
+			    
+		        if (defaultFactory == null && fileFactory == null) {
+		        	throw new Exception("Unknown File processor: " + plugin);   	
+		        }
+			    
+		        if (defaultFactory != null) {
+				    final DefaultPluginInterface processor = defaultFactory.build();
+				    log.debug("Running plugin: {}", defaultFactory.name());
+				    processor.run(element, path, defaults, defines);
+		        }
+		        
+		        if (fileFactory != null) {
+				    final FilePluginInterface processor = fileFactory.build();
+				    log.debug("Running plugin: {}", fileFactory.name());
+				    files.add(processor.getFile(element, path, defaults, defines));
+		        }
+			}
 		}
 		
+		// concat ressources
+		String filename = path + File.separator + Settings.values.get("build.dir") + File.separator + String.valueOf(java.lang.System.nanoTime()) + ".asm";
+		File tmpfile = new File(filename);		
 		
-		return finalbin;
+		for (File file : files) {
+			String fileStr = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+			FileUtils.write(tmpfile, fileStr, StandardCharsets.UTF_8, true);
+		}
+		
+		// assemble
+		return LwAssembler.assemble(tmpfile.getAbsolutePath(), path, defines.values, format);
 	}
 }
