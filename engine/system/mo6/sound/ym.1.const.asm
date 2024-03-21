300775b7a7298a1445f4187cb27bea8b73a49298
bhrousseau <bh.rousseau@gmail.com>
2024-03-21 20:58:20 +0100

    sound demo map to std ti and ym port

--- engine/system/mo6/sound/ym.1.const.asm
+++ engine/system/mo6/sound/ym.1.const.asm
@@ -2,6 +2,6 @@ map.YM2413.A EXPORT
 map.YM2413.D EXPORT
 
  SECTION _constant
-map.YM2413.A        equ $A7F8
-map.YM2413.D        equ $A7F9
+map.YM2413.A        equ $A7F4
+map.YM2413.D        equ $A7F5
  ENDSECTION
--- engine/system/to8/sound/ym.1.const.asm
+++ engine/system/to8/sound/ym.1.const.asm
@@ -2,6 +2,6 @@ map.YM2413.A EXPORT
 map.YM2413.D EXPORT
 
  SECTION _constant
-map.YM2413.A        equ $E7F8
-map.YM2413.D        equ $E7F9
+map.YM2413.A        equ $E7F4
+map.YM2413.D        equ $E7F5
  ENDSECTION
