8c8164d4760345c38ebc833b61b6f94b60a96943
bhrousseau <bh.rousseau@gmail.com>
2024-07-31 14:06:16 +0200

    Fix audio play in mea8000 emul

--- toolbox/debug/src/main/java/com/widedot/toolbox/debug/util/SoundPlayerBlock.java
+++ toolbox/debug/src/main/java/com/widedot/toolbox/debug/util/SoundPlayerBlock.java
@@ -4,25 +4,27 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.SourceDataLine;
 
+import com.widedot.toolbox.debug.ui.Mea8000Device;
+
+import lombok.extern.slf4j.Slf4j;
+
 public class SoundPlayerBlock {
 
 	public final static int SAMPLE_RATE = 64000; // Hz
 	public final static int BIT_DEPTH = 16;      // bits
 	
-	private AudioFormat af;
+	private AudioFormat af = new AudioFormat((float)SAMPLE_RATE, BIT_DEPTH, 1, true, false);	
 	private SourceDataLine sdl;
 	
-	public void play (byte[] audio_buffer) {
+	public void play (byte[] audioBuffer) {
 	    try {		
-		    af = new AudioFormat((float)SAMPLE_RATE, BIT_DEPTH, 1, true, false);
 			sdl = AudioSystem.getSourceDataLine(af);
 			sdl.open();
 		    sdl.start();
-		    
-		    sdl.write(audio_buffer, 0, audio_buffer.length/2);
-			
-			sdl.stop();
-			sdl.flush();
+		    sdl.write(audioBuffer, 0, audioBuffer.length);
+		    sdl.drain();
+		    sdl.stop();
+		    sdl.close();
 		    
 		} catch (Exception e) {
 			e.printStackTrace();
